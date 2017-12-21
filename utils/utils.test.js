97d4ea6e33fd43da0754bf441343d328e2544088
Konstantin Baierer <unixprog@gmail.com>
2017-12-21 20:49:00 +0100

    fix package.json

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,7 +1,8 @@
 const tap = require('tap')
+const utils = require('./dist/utils')
 
 tap.test('truthy', t => {
-  const {truthy} = require('.')
+  const {truthy} = utils
   t.equals(truthy('true'), true, "'true' -> true")
   t.equals(truthy('false'), false, "'false' -> false")
   t.equals(truthy('1'), true, "'true' -> true")
@@ -12,7 +13,7 @@ tap.test('truthy', t => {
 })
 
 tap.test('lastUrlSegment', t => {
-  const {lastUrlSegment} = require('.')
+  const {lastUrlSegment} = utils
   t.equals(lastUrlSegment('http://foo/bar',   false), 'bar', "http://foo/bar   -> 'bar'")
   t.equals(lastUrlSegment('http://foo/bar/',  false), '',    "http://foo/bar/  -> ''")
   t.equals(lastUrlSegment('http://foo/bar/',  true),  'bar', "http://foo/bar/  -> 'bar' (removeTrailing)")
