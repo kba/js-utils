346d8cba7ecec12dfd6d2ec5ce7b3eb301a8cadb
Konstantin Baierer <unixprog@gmail.com>
2018-01-17 04:29:08 +0100

    idiomaticFetch: return response even for reject

--- utils/utils.js
+++ utils/utils.js
@@ -193,7 +193,7 @@ function idiomaticFetch(url, options={}, format='json') {
       } else {
         resp.text().then(bodyData => {
           Object.assign(resp, {bodyData})
-          reject(bodyData)
+          reject(resp)
         })
       }
     }).catch(reject)
