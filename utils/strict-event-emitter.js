fa5e2f26a5088d91a1983b414d0b153999c6f86e
Konstantin Baierer <unixprog@gmail.com>
2018-11-11 12:31:49 +0100

    striceventemitter: log in browser and console

--- utils/strict-event-emitter.js
+++ utils/strict-event-emitter.js
@@ -1,5 +1,11 @@
 const {EventEmitter} = require('eventemitter3')
 
+const colorMap = {
+  on: {node: 88, browser: '#870000'},
+  once: {node: 91, browser: '#8700af'},
+  emit: {node: 23, browser: '#005f5f'},
+}
+
 module.exports = class StrictEventEmitter extends EventEmitter {
 
   constructor(eventNames=[], LOGEVENTS) {
@@ -14,32 +20,43 @@ module.exports = class StrictEventEmitter extends EventEmitter {
     this.eventNames.add(eventName)
   }
 
-  _logEvent(event, emitOrOn, color) {
+  _logEvent(event, emitOrOn, args) {
     const timeStr = new Date().toISOString().slice(11)
-    console.log([
-      `\x1b[38;5;${color}m`,
-      `[${emitOrOn}]`,
-      this.constructor.name,
-      `"${event}"`,
-      timeStr,
-      `\x1b[0m`,
-    ].join(' '))
+    const isNode = typeof window === undefined
+    if (isNode)
+      console.log([
+        `\x1b[38;5;${colorMap[emitOrOn].node}m`,
+        timeStr,
+        `[${emitOrOn}]`,
+        this.constructor.name,
+        `"${event}"`,
+        `\x1b[0m`,
+        args.map(a => a.toString())
+      ].join(' '))
+    else
+      console.log([
+        `%c[${emitOrOn}]`,
+        timeStr,
+        this.constructor.name,
+        `"${event}"`,
+        args.map(a => a.toString())
+      ].join(' '), `color: ${colorMap[emitOrOn].browser}`)
   }
 
   on(event, ...args) {
-    if (this.LOGEVENTS) this._logEvent(event, 'on  ', 88)
+    if (this.LOGEVENTS) this._logEvent(event, 'on', args)
     if (this.eventNames.has(event)) super.on(event, ...args)
     else throw new Error(`Event '${event}' not emitted by ${this.constructor.name}`)
   }
 
   once(event, ...args) {
-    if (this.LOGEVENTS) this._logEvent(event, 'once', 91)
+    if (this.LOGEVENTS) this._logEvent(event, 'once', args)
     if (this.eventNames.has(event)) super.once(event, ...args)
     else throw new Error(`Event '${event}' not emitted by ${this.constructor.name}`)
   }
 
   emit(event, ...args) {
-    if (this.LOGEVENTS) this._logEvent(event, 'emit', 23)
+    if (this.LOGEVENTS) this._logEvent(event, 'emit', args)
     if (this.eventNames.has(event)) super.emit(event, ...args)
     else throw new Error(`Event '${event}' not emitted by ${this.constructor.name}`)
   }
